5e1e3c72d600e831939795db730477add52d8832
root <root@DESKTOP-KALQFR4.localdomain>
2021-06-01 14:33:40 -0400

    dasd

--- myApp.js
+++ myApp.js
@@ -34,7 +34,15 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err,data) => {
+    if(err) {
+      console.log(err)
+      done(err);
+    }
+    else {
+      done(null,data)
+    }
+  });
 };
 
 const findPeopleByName = (personName, done) => {
