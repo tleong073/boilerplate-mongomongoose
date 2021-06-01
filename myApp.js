4261cffd5e218d174997e328c130afa33c43b974
root <root@DESKTOP-KALQFR4.localdomain>
2021-06-01 16:34:42 -0400

    adsas

--- myApp.js
+++ myApp.js
@@ -105,8 +105,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove},(err,res) => {
+    handleErrors(err,res,done);
+  });
 };
 
 const queryChain = (done) => {
