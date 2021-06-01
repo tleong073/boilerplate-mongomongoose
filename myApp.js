970d83165d5bac8b0202fcf83c26a446072642ef
root <root@DESKTOP-KALQFR4.localdomain>
2021-06-01 13:12:24 -0400

    set up complete

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
 let Person;
 
 const createAndSavePerson = (done) => {
